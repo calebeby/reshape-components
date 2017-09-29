253c3e13d2713da6fa0d8b4d5ba77f52f3c5e777
Caleb Eby <caleb.eby01@gmail.com>
2017-09-29 16:42:15 -0700

    fix(lib): remove <import> node when done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const path = require('path')
-const { readFile } = require('mz/fs')
-const { modifyNodes } = require('reshape-plugin-util')
+const path = require("path");
+const { readFile } = require("mz/fs");
+const { modifyNodes } = require("reshape-plugin-util");
 
-const plugin = 'reshape-components'
-const pReduce = require('p-reduce')
+const plugin = "reshape-components";
+const pReduce = require("p-reduce");
 
 const compileComponent = (src, ctx, props) =>
-  readFile(src, 'utf8')
+  readFile(src, "utf8")
     .then(ctx.parser)
     .then(componentTree =>
       pReduce(
@@ -19,20 +19,20 @@ const compileComponent = (src, ctx, props) =>
       const expression = `function () {
         const props = ${JSON.stringify(props)}
         return __nodes[0]
-      }()`
+      }()`;
       return {
-        type: 'code',
+        type: "code",
         content: expression,
         nodes: [codeContent]
-      }
-    })
+      };
+    });
 
 const getImportedComponents = (ast, ctx, opts) => {
-  const importedComponents = {}
-  const promises = []
+  const importedComponents = {};
+  const promises = [];
   return modifyNodes(
     ast,
-    n => n.name === 'import',
+    n => n.name === "import",
     node => {
       // if there is no src, throw an error
       if (!(node.attrs && node.attrs.src)) {
@@ -40,46 +40,50 @@ const getImportedComponents = (ast, ctx, opts) => {
           message: 'import tag has no "src" attribute',
           plugin,
           location: node.location
-        })
+        });
       }
       if (!(node.attrs && node.attrs.as)) {
         throw new ctx.PluginError({
           message: 'import tag has no "as" attribute',
           plugin,
           location: node.location
-        })
+        });
       }
       const resolveRoot =
-        opts.root || (ctx.filename && path.dirname(ctx.filename)) || ''
-      const src = path.join(resolveRoot, node.attrs.src[0].content)
-      const name = node.attrs.as[0].content
+        opts.root || (ctx.filename && path.dirname(ctx.filename)) || "";
+      const src = path.join(resolveRoot, node.attrs.src[0].content);
+      const name = node.attrs.as[0].content;
       promises.push(
         compileComponent(src, ctx).then(component => {
-          importedComponents[name.toLowerCase()] = component
+          importedComponents[name.toLowerCase()] = component;
         })
-      )
-      return null
+      );
+      return "";
     }
   )
-    .then(() => Promise.all(promises))
-    .then(() => importedComponents)
-}
+    .then(newAst => Promise.all(promises).then(() => newAst))
+    .then(ast => ({ importedComponents, ast }));
+};
 
 const main = (opts = {}) => (ast, ctx) => {
-  return getImportedComponents(ast, ctx, opts).then(importedComponents =>
+  return getImportedComponents(
+    ast,
+    ctx,
+    opts
+  ).then(({ importedComponents, ast }) =>
     modifyNodes(
       ast,
       n => n.name in importedComponents,
       node => {
-        const props = { children: ctx.generator(node.content)() }
+        const props = { children: ctx.generator(node.content)() };
         Object.keys(node.attrs).map(prop => {
-          const value = node.attrs[prop]
-          props[prop] = ctx.generator(value)()
-        })
-        return importedComponents[node.name](props)
+          const value = node.attrs[prop];
+          props[prop] = ctx.generator(value)();
+        });
+        return importedComponents[node.name](props);
       }
     )
-  )
-}
+  );
+};
 
-module.exports = main
+module.exports = main;
